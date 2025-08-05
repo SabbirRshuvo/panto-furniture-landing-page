f9b2be3cb79bb8ff3653a4ab386d6673864c4e82
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-08-06 03:03:31 +0600

    adding the color hover section

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -14,14 +14,30 @@ const Banner = () => {
           Turn your room with panto into a lot more minimalist and modern with
           ease and speed
         </p>
-        <div className="border  border-gray-400 bg-gray-400  z-10 w-70 h-8 mx-auto relative items-center rounded-2xl md:w-110 sm:w-96 mt-10">
+        {/* search field */}
+        <div className="border  border-gray-400 bg-gray-100   z-10 w-70 h-8 mx-auto relative items-center rounded-2xl md:w-110 sm:w-96 mt-10">
           <input
             type="text"
             placeholder="Search Furniture..."
-            className="focus:outline-none px-6 text-sm text-start py-1"
+            className="focus:outline-none px-6 text-sm text-start py-1 text-black"
           />
           <div className="absolute top-1/2 -translate-1/2 -right-4 bg-amber-400 p-2 rounded-full ">
             <FaSearch />
+          </div>
+        </div>
+        {/* hover color section */}
+
+        <div className="absolute bottom-20">
+          <div className="relative group flex items-center justify-center h-20">
+            {/* hidden color section */}
+            <div className="absolute top-[-10px] opacity-0 flex gap-2 group-hover:opacity-100 transition-opacity duration-300">
+              <div className="bg-red-500 w-4 h-4 rounded-full" />
+              <div className="bg-green-500 w-4 h-4 rounded-full" />
+              <div className="bg-blue-500 w-4 h-4 rounded-full" />
+            </div>
+
+            {/* bg section */}
+            <div className="bg-gray-300  w-10 h-10 rounded-full" />
           </div>
         </div>
       </div>
