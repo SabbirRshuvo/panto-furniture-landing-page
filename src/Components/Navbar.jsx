3219c7cc6aa4ffd44481ef0c38c340f2c6c44389
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-08-05 23:38:03 +0600

    navbar link add

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,29 +3,32 @@ import { FaCartShopping } from "react-icons/fa6";
 import { NavLink } from "react-router";
 
 const Navbar = () => {
-  const navlinks = [
+  const navLinks = [
     { name: "Furniture", path: "/furniture" },
     { name: "Shop", path: "/shop" },
     { name: "About Us", path: "/about-us" },
     { name: "Contact", path: "/contact" },
   ];
   return (
-    <div>
+    <div className="max-w-screen-2xl mx-auto flex justify-between items-center z-10">
       {/* logo */}
       <div>
-        <h2>Panto</h2>
+        <h2 className="text-2xl font-semibold">Panto</h2>
       </div>
       {/* links */}
-      <div>
-        {navlinks.map((item, index) => (
-          <NavLink to={item.path} key={index}>
+      <ul className="flex">
+        {navLinks.map((item, index) => (
+          <NavLink to={item.path} key={index} className="ml-6">
             {item.name}
           </NavLink>
         ))}
-      </div>
+      </ul>
       {/* cart icon */}
-      <div>
-        <FaCartShopping />
+      <div className="relative">
+        <div className="text-2xl ">
+          <FaCartShopping />
+        </div>
+        <span className="absolute bottom-2 left-4">0</span>
       </div>
     </div>
   );
